b8581cfaaf725a6e9fe2db6979f1111f6a72ae41
Navya Sri Akula <150413357+23wh1a12b6@users.noreply.github.com>
2025-07-21 19:28:42 +0530

    ugly-number.java

---
+++ Leetcode/ugly-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isUgly(int n) {
+      if (n==0){
+        return false;
+      }
+      if(n==1){
+             return true;
+      }
+      int []arr={2,3,5};
+      for(int i=0;i<3;i++){
+        while(n%arr[i]==0){
+          n/=arr[i];
+        }
+      }
+      return n==1;
+        
+    }
+}
